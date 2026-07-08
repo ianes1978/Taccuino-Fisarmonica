8292476be8deaa8cfc89f773e2231ba05b244fe0
ianes1978 <ianes1978@users.noreply.github.com>
2026-07-08 15:01:25 +0000

    deploy: c244f4d37eaaae83c83a3cc9b93e18afc57422cd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4082018801" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3037423508" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
